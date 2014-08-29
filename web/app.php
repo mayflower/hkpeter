f5fd1f38848ef4d08cea5aaa7f106aa2b2961592
elseym <elseym@me.com>
2014-08-29 11:39:07 +0200

    consolidate front controllers

--- web/app.php
+++ web/app.php
@@ -2,27 +2,35 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+umask(0000);
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$apcLoader = new ApcClassLoader('sf2', $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+$env = (isset($_SERVER['HTTP_HOST']) && '.dev' === substr($_SERVER['HTTP_HOST'], -4)) ? 'dev' : 'prod';
+$useCache = !('dev' === $env && isset($_GET['disableCache']) && !!($_GET['disableCache']));
+$loader = require_once __DIR__.'/../app/' . ($useCache ? 'bootstrap.php.cache' : 'autoload.php');
 
-require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+if ('dev' === $env) {
+    Debug::enable();
+} elseif ('prod' === $env) {
+    $apcLoader = new ApcClassLoader('sf2', $loader);
+    $loader->unregister();
+    $apcLoader->register(true);
+}
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+require_once __DIR__ . '/../app/AppKernel.php';
+$kernel = new AppKernel($env, 'dev' === $env);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
+if ('dev' !== $env) {
+    $kernel->loadClassCache();
+}
+
+if ('prod' === $env) {
+    require_once __DIR__.'/../app/AppCache.php';
+    $kernel = new AppCache($kernel);
+    Request::enableHttpMethodParameterOverride();
+}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
