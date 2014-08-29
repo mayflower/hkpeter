9a8451154d1b10d6656efe949b67e8a45b5106d2
elseym <elseym@me.com>
2014-08-29 11:40:01 +0200

    add portalcontroller and view

--- src/elseym/HKPeterBundle/Controller/PortalController.php
+++ src/elseym/HKPeterBundle/Controller/PortalController.php
@@ -2,11 +2,35 @@
 
 namespace elseym\HKPeterBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Class PortalController
  * @package elseym\HKPeterBundle\Controller
  */
 class PortalController extends AbstractController
 {
+    /** @var EngineInterface $templating */
+    private $templating;
 
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function portalAction(Request $request)
+    {
+        return $this->templating->renderResponse("elseymHKPeterBundle:portal:index.html.twig");
+    }
+
+    /**
+     * @param EngineInterface $templating
+     * @return $this
+     */
+    public function setTemplating($templating)
+    {
+        $this->templating = $templating;
+        return $this;
+    }
 }
