59ca9ef240929854a0cf18e370d2f014bd625df3
elseym <elseym@me.com>
2014-09-05 18:22:49 +0200

    parse gpg exported armored keys

--- src/elseym/HKPeterBundle/Model/Key.php
+++ src/elseym/HKPeterBundle/Model/Key.php
@@ -8,6 +8,11 @@ namespace elseym\HKPeterBundle\Model;
  */
 class Key
 {
+    const KIND_PUB = 'pub';
+    const KIND_SUB = 'sub';
+    const KIND_SEC = 'sec';
+    const KIND_SSB = 'ssb';
+
     /** @var string $id */
     private $id;
 
@@ -32,6 +37,72 @@ class Key
         $this->email = $email;
         $this->fingerprint = $fingerprint;
         $this->content = $content;
+    }
+
+    /**
+     * @param $keyString
+     */
+    public static function createFromString($keyString)
+    {
+        $fields = [
+            'uid' => [''],
+            self::KIND_PUB => ['id', 'bits', 'algo', 'year', 'month', 'day', 'flag', 'flagyear', 'flagmonth', 'flagday']
+        ];
+
+        $fields[self::KIND_SUB] = $fields[self::KIND_PUB];
+        $fields[self::KIND_SEC] = $fields[self::KIND_PUB];
+        $fields[self::KIND_SSB] = $fields[self::KIND_PUB];
+
+        $flagRegex = "(?:\\[(?'flag'(?:expire.:|revoked|expired|disabled))(?: (?'flagyear'\\d{4})-(?'flagmonth'\\d\\d)-(?'flagday'\\d\\d))\\])?";
+        $metaRegex = "(?'bits'\\d+)(?'algo'\\S)\\/(?'id'[0-9A-Fa-f]+)(?!.*[<@>])";
+        $nameRegex = "(?'uid'(?'name'.+?)\\<(?'email'[^@]+@.+\\.\\w+)\\>)";
+        $kindRegex = "(?'kind'" . implode("|", array_keys($fields)) . ")";
+        $pubSubRegex = "/" . $kindRegex . "\\s+(?:" . $metaRegex . "\\s+(?'year'\\d{4})-(?'month'\\d\\d)-(?'day'\\d\\d)\\s*" . $flagRegex ."|" . $nameRegex . ")/";
+
+        $rawKeys = [];
+        $keyIndex = 0;
+
+        $keyLines = explode("\n", $keyString);
+        foreach ($keyLines as $keyLine) {
+            if (!preg_match($pubSubRegex, $keyLine, $matches)) {
+                continue;
+            }
+
+            $data = self::extractFields($fields[$matches['kind']], $matches);
+            switch ($matches['kind']) {
+                case self::KIND_PUB:
+                case self::KIND_SEC:
+                    $keyIndex++;
+                    $rawKeys[$keyIndex] = $data;
+                    break;
+                case self::KIND_SUB:
+                case self::KIND_SSB:
+                    $rawKeys[$keyIndex]['subs'][] = $data;
+                    break;
+                case "uid":
+                    $rawKeys[$keyIndex]['uids'][] = $data;
+                    break;
+            }
+        }
+
+        var_dump($rawKeys);
+    }
+
+    /**
+     * @param array $fields
+     * @param array $source
+     * @return array
+     */
+    private static function extractFields($fields, $source)
+    {
+        $destination = [];
+        foreach($fields as $field) {
+            if (isset($source[$field])) {
+                $destination[$field] = $source[$field];
+            }
+        }
+
+        return $destination;
     }
 
     /**
