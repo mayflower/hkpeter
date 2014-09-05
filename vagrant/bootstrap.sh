badf3f4f7db4b07eb0e5038f4b601107e89440a3
elseym <elseym@me.com>
2014-09-05 12:32:18 +0200

    fix bootstrapping: ensure .vagrant directory be present

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -14,11 +14,11 @@ fi
 readonly DIR
 
 main() {
+    (mkdir vagrant/.vagrant 2>/dev/null || true)
     echo "[bootstrap] link Vagrantfile to project root" && \
         ln -si vagrant/Vagrantfile "$DIR/../Vagrantfile"
     echo "[bootstrap] link .vagrant to project root" && \
-        ln -si vagrant/.vagrant "$DIR/../.vagrant" &&
-        (mkdir vagrant/.vagrant 2>/dev/null || true)
+        ln -si vagrant/.vagrant "$DIR/../.vagrant"
     echo "[bootstrap] create devstack.yaml in project root" && \
         cp -i "$DIR/devstack.yaml.dist" "$DIR/../devstack.yaml"
 }
