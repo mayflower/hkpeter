92544e98dc56aadf7ade995517818cba30d22d16
midler <marcel.idler@mayflower.de>
2015-05-15 09:50:04 +0200

    create add_key form & service

---
+++ src/elseym/HKPeterBundle/Form/Type/AddKeyType.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class AddKeyType
+ */
+class AddKeyType extends \Symfony\Component\Form\AbstractType
+{
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('armoredKey','textarea', array(
+                'mapped' => false,
+            ))
+            ->add('save', 'submit')
+        ;
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'add_key';
+    }
+
+}
